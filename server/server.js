ba45dcfc35a4b7fff058d5577f24534015142486
Vaishnavi Singh <vaishnavisinghh1409@gmail.com>
2026-06-08 17:57:29 +0530

    Add root startup config and fix server dotenv and Cloudinary import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors    = require('cors');
-require('dotenv').config();
+const path    = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const connectDB = require('./config/db');
 
 const app = express();
